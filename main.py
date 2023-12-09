5c0618d8fb681791093faf48983beaccd972fbaf
Acer <oduvan4ik_888@mail.ru>
2023-12-09 21:47:24 +0300

    Пространство имен

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
 
+test_function()
+#inner_function() # Результат выполнения программы:
+# NameError: name 'inner_function' is not defined
